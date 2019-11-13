9ce5de3c9d30502b014d55651a4148bcca3e7d49
DimejiAre <olardi2@gmail.com>
2019-11-13 14:50:20 +0100

    create endpoint for login with token generation

--- helpers/dbModels.js
+++ helpers/dbModels.js
@@ -2,10 +2,20 @@ const db = require('../data/dbConfig');
 const bcrypt = require('bcryptjs');
 
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
 
 function registerUser(user){
     user.password = bcrypt.hashSync(user.password)
     return db('users').insert(user)
+}
+
+async function loginUser(user){
+    let result = await db('users').where({username: user.username}).first()
+    if (result && bcrypt.compareSync(user.password, result.password)){
+        return result
+    } else {
+        return null
+    }
 }
--- routers/auth.js
+++ routers/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../helpers/dbModels');
 
+const generateToken = require('../helpers/generateToken');
+
 router.post('/register', async (req,res) => {
     try{
         const user = req.body;
@@ -10,6 +12,25 @@ router.post('/register', async (req,res) => {
     }
     catch (error){
         res.status(500).json({error: `An error occurred during registeration. ${error}`})
+    }
+})
+
+router.post('/login', async (req,res) => {
+    try{
+        const payload = req.body;
+        let user = await db.loginUser(payload)
+        if(user){
+            const token = generateToken(user)
+            res.status(201).json({
+                message: `Welcome ${user.username}`,
+                token: token
+            })
+        } else {
+            res.status(404).json({message: `invalid credentials`})
+        }
+    }
+    catch (error){
+        res.status(500).json({error: `An error occurred while logging in. ${error}`})
     }
 })
 
